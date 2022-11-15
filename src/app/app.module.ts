7da9a903df10d48cbd84878cde063798a2351bb3
Emily <emilldr>
2022-11-14 21:48:47 -0600

    setup layout & some concepts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProcessListComponent } from './components/process-list/process-list.component';
+import { ProcessComponent } from './components/process/process.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StageComponent } from './components/stage/stage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProcessListComponent,
+    ProcessComponent,
+    NavbarComponent,
+    StageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
